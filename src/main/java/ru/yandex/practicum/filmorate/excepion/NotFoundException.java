593fdb34a080b5202d6171bed7b63e73403389cb
David Egorov <egor0vdavid@yandex.ru>
2024-07-14 23:28:19 +0300

    First iteration

--- src/main/java/ru/yandex/practicum/filmorate/excepion/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/excepion/NotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.excepion;
 
-public class NotFoundException extends RuntimeException{
+public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
     }
