e3cc36942fc93d6a3abbe4aebb9ddc8b519f2733
David Egorov <egor0vdavid@yandex.ru>
2024-07-23 22:05:44 +0300

    Учёл замечания, реализовал сервисы для храненния данных.

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -0,0 +1,86 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.excepion.ConditionsMetException;
+import ru.yandex.practicum.filmorate.excepion.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class FilmService {
+
+    private static final Logger log = LoggerFactory.getLogger(FilmController.class);
+
+    private final Map<Long, Film> films = new HashMap<>();
+
+    public Collection<Film> getAll() {
+        log.info("Запрошен список всех фильмов");
+        return films.values();
+    }
+
+    public Film create(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            log.error("name = null");
+            throw new ConditionsMetException("название не может быть пустым");
+
+        }
+        if (film.getDescription().length() > 200) {
+            log.error("Description.length > 200");
+            throw new ConditionsMetException("максимальная длина описания - 200 символов");
+        }
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.error("Release date is before 1895");
+            throw new ConditionsMetException("дата релиза - не раньше 28 декабря 1895 года");
+        }
+        if (film.getDuration() < 0) {
+            log.error("Duration is negative");
+            throw new ConditionsMetException("продолжительность фильма должна быть положительным числом");
+        }
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("фильм добавлен: " + film);
+        return film;
+    }
+
+    public Film update(Film newFilm) {
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            if (newFilm.getId() == null) {
+                log.error("id = null");
+                throw new ConditionsMetException("id должен быть указан");
+            }
+            if (newFilm.getName() != null || newFilm.getName().isBlank()) {
+                oldFilm.setName(newFilm.getName());
+            }
+            if (newFilm.getDescription() != null && newFilm.getDescription().length() < 200) {
+                oldFilm.setDescription(newFilm.getDescription());
+            }
+            if (newFilm.getDuration() != null && newFilm.getDuration() > 0) {
+                oldFilm.setDuration(newFilm.getDuration());
+            }
+            if (newFilm.getReleaseDate() != null && newFilm.getReleaseDate().isAfter(LocalDate.of(1895, 12, 28))) {
+                oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            }
+            log.info("фильм обновлен: " + oldFilm.toString());
+            return oldFilm;
+        }
+        throw new NotFoundException("фильм с id = " + newFilm.getId() + " не найден");
+    }
+
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,80 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.excepion.NotFoundException;
+import ru.yandex.practicum.filmorate.excepion.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserService {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    public Collection<User> getAll() {
+        log.info("Запрошен список всех пользователей");
+        return users.values();
+    }
+
+    public User createUser(User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.error("email = null or not found @");
+            throw new ValidationException("электронная почта не может быть пустой и должна содержать символ @");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            log.error("login = null");
+            throw new ValidationException("логин не может быть пустым и не должен содержать пробелы");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.error("Birthday in the future");
+            throw new ValidationException("дата рождения не может быть в будущем");
+        }
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        log.info("Пользователь создан: " + user);
+        return user;
+    }
+
+    public User update(User newUser) {
+        if (users.containsKey(newUser.getId())) {
+            User oldUser = users.get(newUser.getId());
+            // обновление поля email
+            if (newUser.getEmail() != null) {
+                oldUser.setEmail(newUser.getEmail());
+            }
+            if (newUser.getName() != null) {
+                oldUser.setName(newUser.getName());
+            }
+            if (newUser.getLogin() != null) {
+                oldUser.setLogin(newUser.getLogin());
+            }
+            if (newUser.getBirthday() != null && newUser.getBirthday().isBefore(LocalDate.now())) {
+                oldUser.setBirthday(newUser.getBirthday());
+            }
+            log.info("Пользователь обновлён: " + oldUser.toString());
+            return oldUser;
+        }
+        log.error("user with id = " + newUser.getId() + " not found");
+        throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
